f2beddbb6e1914a373cbc601a87a66e6687740ae
dharudharanib-balu <dharudharanib@gmail.com>
2026-09-16 19:44:07 +0530

    Add files via upload

---
+++ InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Verifies that the Spring Boot application context starts successfully
+    }
+}
